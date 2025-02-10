f0022c311067d99f3c8bc4b78e4c16a3f2668882
Michaela Hunt <micapplehunt@gmail.com>
2025-02-10 13:45:34 -0500

    Complete queries on client side

--- client/src/utils/queries.ts
+++ client/src/utils/queries.ts
@@ -1,10 +1,62 @@
 import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
-  query ? {
-    ? {
+  query me() {
+    user {
+        _id
+        username
+        email
+        bookCount
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
       _id
-      name
+      email
+      username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($input: AddUserArgs) {
+    addUser(input: $input) {
+      token
+      user {
+      _id
+      email
+      username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: SaveBookArgs) {
+    saveBook(input: $input) {
+      user {
+      _id
+      email
+      username
+      }
+    }
+  } 
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      User {
+      _id
+      email
+      username
+      }
     }
   }
 `;
